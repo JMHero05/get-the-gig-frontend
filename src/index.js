bb2c1eab09645cdd21a13097f270b8ce546a715a
Jake Mills <jake.mills.05@gmail.com>
2020-10-11 17:44:18 -0400

    Removed redux provider and store info, and started routes in App

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const store = createStore();
+// const store = createStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+  // <Provider>
+  <App />,
+  /* </Provider>, */ document.getElementById('root')
 );
