e17ce40ac423e7f5beea6c87e4b5553b1b5022ac
Jake Mills <jake.mills.05@gmail.com>
2020-10-12 18:06:11 -0400

    Added state for Casting Registration Form

--- src/components/CastingRegistration.js
+++ src/components/CastingRegistration.js
@@ -3,44 +3,93 @@ import { Form, Col, Button, Container } from 'react-bootstrap';
 import { states } from '../helpers/state.js';
 
 class CastingRegistration extends Component {
+  state = {
+    name: '',
+    agency: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+    email: '',
+    password: '',
+  };
+
+  handleChange = (e) => {
+    console.log(e.target.name);
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let casting = {
+      name: this.state.name,
+      agency: this.state.agency,
+      address: this.state.address,
+      city: this.state.city,
+      state: this.state.state,
+      zip: this.state.zip,
+      email: this.state.email,
+      password: this.state.password,
+    };
+    console.log(casting);
+  };
+
   render() {
     return (
       <Container className='m-5'>
         <h1>Casting Director Registration</h1>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Row>
             <Form.Group as={Col} controlId='formGridName'>
               <Form.Label>Name</Form.Label>
-              <Form.Control name='name' placeholder='Enter Name' />
+              <Form.Control
+                name='name'
+                placeholder='Enter Name'
+                onChange={this.handleChange}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridAgency'>
               <Form.Label>Agency Name</Form.Label>
-              <Form.Control name='agency' placeholder='Agency Name' />
+              <Form.Control
+                name='agency'
+                placeholder='Agency Name'
+                onChange={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId='formGridAddress1'>
             <Form.Label>Address</Form.Label>
-            <Form.Control placeholder='1234 Main St' />
+            <Form.Control
+              name='address'
+              placeholder='1234 Main St'
+              onChange={this.handleChange}
+            />
           </Form.Group>
 
           <Form.Row>
             <Form.Group as={Col} controlId='formGridCity'>
               <Form.Label>City</Form.Label>
-              <Form.Control />
+              <Form.Control name='city' onChange={this.handleChange} />
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridState'>
               <Form.Label>State</Form.Label>
-              <Form.Control as='select' defaultValue='Choose...'>
+              <Form.Control
+                as='select'
+                defaultValue='Choose...'
+                name='state'
+                onChange={this.handleChange}>
                 {states}
               </Form.Control>
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridZip'>
               <Form.Label>Zip</Form.Label>
-              <Form.Control />
+              <Form.Control name='zip' onChange={this.handleChange} />
             </Form.Group>
           </Form.Row>
 
@@ -51,6 +100,7 @@ class CastingRegistration extends Component {
                 name='email'
                 type='email'
                 placeholder='Enter Email'
+                onChange={this.handleChange}
               />
             </Form.Group>
 
@@ -60,6 +110,7 @@ class CastingRegistration extends Component {
                 name='password'
                 type='password'
                 placeholder='Password'
+                onChange={this.handleChange}
               />
             </Form.Group>
           </Form.Row>
