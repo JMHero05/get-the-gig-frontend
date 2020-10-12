311df2fd1cd4006a6272e599d8d64016ec507d52
Jake Mills <jake.mills.05@gmail.com>
2020-10-12 18:13:26 -0400

    Added state for Actor Registration Form

--- src/components/ActorRegistration.js
+++ src/components/ActorRegistration.js
@@ -4,58 +4,118 @@ import { states } from '../helpers/state';
 import { gender } from '../helpers/gender';
 
 class ActorRegistration extends Component {
+  state = {
+    first_name: '',
+    last_name: '',
+    gender: '',
+    image: '',
+    address: '',
+    city: '',
+    state: '',
+    zip: '',
+    email: '',
+    password: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let actor = {
+      first_name: this.state.first_name,
+      last_name: this.state.last_name,
+      gender: this.state.gender,
+      image: this.state.image,
+      address: this.state.address,
+      city: this.state.city,
+      state: this.state.state,
+      zip: this.state.zip,
+      email: this.state.email,
+      password: this.state.password,
+    };
+    console.log(actor);
+  };
+
   render() {
     return (
       <Container className='m-5'>
         <h1>Actor Registration</h1>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <Form.Row>
             <Form.Group as={Col} controlId='formGridFirst'>
               <Form.Label>First Name</Form.Label>
-              <Form.Control name='firstName' placeholder='First Name' />
+              <Form.Control
+                name='first_name'
+                placeholder='First Name'
+                onChange={this.handleChange}
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridAgency'>
               <Form.Label>Last Name</Form.Label>
-              <Form.Control name='lastName' placeholder='Last Name' />
+              <Form.Control
+                name='last_name'
+                placeholder='Last Name'
+                onChange={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Row>
             <Form.Group as={Col} controlId='formGridGender'>
               <Form.Label>Gender</Form.Label>
-              <Form.Control name='gender' as='select' defaultValue='Choose...'>
+              <Form.Control
+                name='gender'
+                as='select'
+                defaultValue='Choose...'
+                onChange={this.handleChange}>
                 {gender}
               </Form.Control>
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridHeadshot'>
               <Form.Label>Headshot</Form.Label>
-              <Form.Control name='headshot' placeholder='Image URL...' />
+              <Form.Control
+                name='image'
+                placeholder='Image URL...'
+                onChange={this.handleChange}
+              />
             </Form.Group>
           </Form.Row>
 
           <Form.Group controlId='formGridAddress1'>
             <Form.Label>Address</Form.Label>
-            <Form.Control placeholder='1234 Main St' />
+            <Form.Control
+              name='address'
+              placeholder='1234 Main St'
+              onChange={this.handleChange}
+            />
           </Form.Group>
 
           <Form.Row>
             <Form.Group as={Col} controlId='formGridCity'>
               <Form.Label>City</Form.Label>
-              <Form.Control />
+              <Form.Control name='city' onChange={this.handleChange} />
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridState'>
               <Form.Label>State</Form.Label>
-              <Form.Control as='select' defaultValue='Choose...'>
+              <Form.Control
+                as='select'
+                defaultValue='Choose...'
+                onChange={this.handleChange}
+                name='state'>
                 {states}
               </Form.Control>
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridZip'>
               <Form.Label>Zip</Form.Label>
-              <Form.Control />
+              <Form.Control onChange={this.handleChange} name='zip' />
             </Form.Group>
           </Form.Row>
 
@@ -66,6 +126,7 @@ class ActorRegistration extends Component {
                 name='email'
                 type='email'
                 placeholder='Enter Email'
+                onChange={this.handleChange}
               />
             </Form.Group>
 
@@ -75,6 +136,7 @@ class ActorRegistration extends Component {
                 name='password'
                 type='password'
                 placeholder='Password'
+                onChange={this.handleChange}
               />
             </Form.Group>
           </Form.Row>
