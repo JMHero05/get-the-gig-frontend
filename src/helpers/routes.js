eb132d73ee537bd1998c525e269e3db120fb26fd
Jake Mills <jake.mills.05@gmail.com>
2020-10-14 20:54:42 -0400

    Added routes helper

---
+++ src/helpers/routes.js
@@ -0,0 +1,5 @@
+export const GIG = 'http://localhost:3000/api/v1/gigs/';
+export const CASTING_SIGN_IN =
+  'http://localhost:3000/api/v1/casting_directors/login/';
+export const CASTING_PROFILE =
+  'http://localhost:3000/api/v1/casting_directors/profile/';
