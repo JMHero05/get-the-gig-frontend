1aabb3930195ee65a9d37d32f36b4f576b6a3aac
Jake Mills <jake.mills.05@gmail.com>
2020-10-14 20:56:32 -0400

    Added actionTypes

---
+++ src/redux/constants/actionTypes.js
@@ -0,0 +1,10 @@
+export const LOGIN_ERROR = 'LOGIN_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const CREATE_GIG_ERROR = 'CREATE_GIG_ERROR';
+export const CREATE_GIG = 'CREATE_GIG';
+export const GET_GIGS = 'GET_GIGS';
+export const GET_GIG = 'GET_GIG';
+export const VALID_TOKEN = 'VALID_TOKEN';
+export const SIGNOUT_SUCCESS = 'SIGNOUT_SUCCESS';
+export const REGISTRATION_ERROR = 'REGISTRATION_ERROR';
+export const REGISTRATION_SUCCESS = 'REGISTRATION_SUCCESS';
