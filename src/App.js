08bc6ef164ba9e60cf9dc503173a9b11b6d52181
Jake Mills <jake.mills.05@gmail.com>
2020-10-14 20:45:09 -0400

    Made App Component a class component, added redux dispatchToProps, added gig routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { validToken } from './redux/actions/authAction';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
@@ -7,26 +9,42 @@ import ActorRegistration from './components/ActorRegistration';
 import SignIn from './components/SignIn';
 import GigNavbar from './components/layout/GigNavbar';
 import GigContainer from './containers/GigContainer';
+import CreateGig from './components/CreateGig';
+import GigDetails from './components/GigDetails';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <GigNavbar />
-      <Switch>
-        <Route path='/home' render={() => <Home />} />
-        <Route path='/signin' render={() => <SignIn />} />
-        <Route
-          path='/casting_director/registration'
-          render={() => <CastingRegistration />}
-        />
-        <Route
-          path='/actor/registration'
-          render={() => <ActorRegistration />}
-        />
-        <Route path='/gigs' render={() => <GigContainer />} />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.validToken();
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <GigNavbar />
+        <Switch>
+          <Route path='/home' render={() => <Home />} />
+          <Route path='/signin' render={() => <SignIn />} />
+          <Route
+            path='/casting_director/registration'
+            render={() => <CastingRegistration />}
+          />
+          <Route
+            path='/actor/registration'
+            render={() => <ActorRegistration />}
+          />
+          <Route path='/gigs/create' render={() => <CreateGig />} />
+          <Route path='/gigs/:id' component={GigDetails} />
+          <Route path='/gigs' render={() => <GigContainer />} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    validToken: () => dispatch(validToken()),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
