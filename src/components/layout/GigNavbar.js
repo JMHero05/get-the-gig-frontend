03641b0bb5b9a98427ef0f394bc49edeab1ecf3f
Jake Mills <jake.mills.05@gmail.com>
2020-10-14 20:52:31 -0400

    Navbar now has hidden routes for Casting

--- src/components/layout/GigNavbar.js
+++ src/components/layout/GigNavbar.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Navbar, Container } from 'react-bootstrap';
 import ActorLinks from './ActorLinks';
 import CastingLinks from './CastingLinks';
 import SignedOutLinks from './SignedOutLinks';
 
-export default function GigNavbar() {
+function GigNavbar(props) {
+  const { user } = props;
+
+  const castingLinks = user.agency ? <CastingLinks /> : <SignedOutLinks />;
+
   return (
-    <Navbar bg='dark' variant='dark' sticky='top' className='mb-5'>
+    <Navbar bg='dark' variant='dark' sticky='top'>
       <Container>
-        <Link to='/home'>
-          <Navbar.Brand>Gig</Navbar.Brand>
-        </Link>
-        <ActorLinks />
-        {/* <CastingLinks /> */}
+        {user ? (
+          <Link to='/gigs'>
+            <Navbar.Brand>Gig</Navbar.Brand>
+          </Link>
+        ) : (
+          <Link to='/home'>
+            <Navbar.Brand>Gig</Navbar.Brand>
+          </Link>
+        )}
+        {/* <ActorLinks /> */}
+        {castingLinks}
         {/* <SignedOutLinks /> */}
       </Container>
     </Navbar>
   );
 }
+
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    user: state.auth.user,
+  };
+};
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps)(GigNavbar);
