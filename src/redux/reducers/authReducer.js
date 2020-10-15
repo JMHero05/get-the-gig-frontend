aa59532989b272d22b6aee0ee282466fa0e5244e
Jake Mills <jake.mills.05@gmail.com>
2020-10-14 20:57:12 -0400

    Added gig and auth reducers

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,7 +1,54 @@
-const initState = {};
+import {
+  LOGIN_ERROR,
+  LOGIN_SUCCESS,
+  VALID_TOKEN,
+  SIGNOUT_SUCCESS,
+  REGISTRATION_ERROR,
+  REGISTRATION_SUCCESS,
+} from '../constants/actionTypes';
+
+const initState = {
+  authError: null,
+  user: '',
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case VALID_TOKEN:
+      return {
+        ...state,
+        user: action.user,
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        authError: 'Login failed',
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        authError: null,
+        user: action.user,
+      };
+    case SIGNOUT_SUCCESS:
+      console.log('signout success');
+      localStorage.removeItem('token');
+      return { ...state, user: '' };
+    case REGISTRATION_ERROR:
+      return {
+        ...state,
+        authError: 'Registration failed',
+      };
+    case REGISTRATION_SUCCESS:
+      console.log(action);
+      return {
+        ...state,
+        authError: null,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
 };
 
 export default authReducer;
--- src/redux/reducers/gigReducer.js
+++ src/redux/reducers/gigReducer.js
@@ -1,61 +1,33 @@
+import {
+  GET_GIGS,
+  GET_GIG,
+  CREATE_GIG,
+  CREATE_GIG_ERROR,
+} from '../constants/actionTypes';
+
 const initState = {
-  gigs: [
-    {
-      id: 1,
-      casting_director_id: 4,
-      title: 'Little Shop of Horrors',
-      gig_type: 'Musical',
-      union: false,
-      producer: 'Brittaney Walter',
-      director: "Henry O'Kon",
-      choreographer: 'Brittni Cummings',
-      music_director: 'Sen. Doreatha Leuschke',
-      opening_date: 'Mon, 01 Feb 2021',
-      closing_date: 'Sat, 17 Apr 2021',
-      gig_location: "Shea's Performing Arts Center",
-      pay_rate: '311/week',
-      audition_date: 'Thu, 19 Nov 2020',
-      audition_location: 'Pearl Studios',
-    },
-    {
-      id: 2,
-      casting_director_id: 2,
-      title: 'Hairspray',
-      gig_type: 'Musical',
-      union: false,
-      producer: 'Jaimie Homenick',
-      director: 'Miss Neil Lakin',
-      choreographer: 'Jessie Willms',
-      music_director: 'Yuk Stroman',
-      opening_date: 'Sun, 28 Mar 2021',
-      closing_date: 'Mon, 24 May 2021',
-      gig_location: "Shea's Performing Arts Center",
-      pay_rate: '538/week',
-      audition_date: 'Tue, 01 Dec 2020',
-      audition_location: 'Pearl Studios',
-    },
-    {
-      id: 3,
-      casting_director_id: 5,
-      title: 'Chicago',
-      gig_type: 'Musical',
-      union: true,
-      producer: 'Nicky Block',
-      director: 'Gov. Andre Auer',
-      choreographer: 'Josh Schaden',
-      music_director: 'Hoa Gislason',
-      opening_date: 'Sun, 24 Jan 2021',
-      closing_date: 'Thu, 03 Jun 2021',
-      gig_location: "Shea's Performing Arts Center",
-      pay_rate: '414/week',
-      audition_date: 'Sat, 21 Nov 2020',
-      audition_location: 'Pearl Studios',
-    },
-  ],
+  gigs: [],
+  gig: null,
 };
 
 const gigReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case GET_GIGS:
+      return { ...state, gigs: action.payload };
+    case GET_GIG:
+      return {
+        ...state,
+        gig: action.payload,
+      };
+    case CREATE_GIG:
+      console.log('created gig', action.gig);
+      return state;
+    case CREATE_GIG_ERROR:
+      console.log('create project error', action.error);
+      return state;
+    default:
+      return state;
+  }
 };
 
 export default gigReducer;
