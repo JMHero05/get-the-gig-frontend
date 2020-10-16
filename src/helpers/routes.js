e19c424410798ad824f75d59c15734a7dcde7c28
Jake Mills <jake.mills.05@gmail.com>
2020-10-16 15:15:26 -0400

    Added Casting and Role links

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,5 +1,6 @@
 export const GIG = 'http://localhost:3000/api/v1/gigs/';
 export const CASTING_SIGN_IN =
   'http://localhost:3000/api/v1/casting_directors/login/';
-export const CASTING_PROFILE =
-  'http://localhost:3000/api/v1/casting_directors/profile/';
+export const CASTING_PROFILE = 'http://localhost:3000/api/v1/casting/profile/';
+export const CASTING = 'http://localhost:3000/api/v1/casting_directors/';
+export const ROLE = 'http://localhost:3000/api/v1/roles/';
