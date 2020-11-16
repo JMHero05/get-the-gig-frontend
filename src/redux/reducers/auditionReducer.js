d8a60503f3d936b61ccdc5a4e7c0f0d31d793de2
Jake Mills <jake.mills.05@gmail.com>
2020-11-16 17:33:56 -0500

    Added creating auditions, patching auditions, and getting gig auditions for audition reducer

--- src/redux/reducers/auditionReducer.js
+++ src/redux/reducers/auditionReducer.js
@@ -1,4 +1,9 @@
-import { GET_ACTOR_AUDITIONS } from '../constants/actionTypes';
+import {
+  GET_ACTOR_AUDITIONS,
+  CREATE_AUDITION,
+  GET_GIG_AUDITIONS,
+  PATCH_AUDITION,
+} from '../constants/actionTypes';
 
 const initState = {
   auditions: [],
@@ -11,6 +16,36 @@ const auditionReducer = (state = initState, action) => {
       return {
         ...state,
         auditions: action.payload,
+      };
+    case CREATE_AUDITION:
+      console.log('CREATED AUDITION', action.audition);
+      return state;
+    case GET_GIG_AUDITIONS:
+      console.log('GET GIG AUDITIONS');
+      return {
+        ...state,
+        auditions: action.payload,
+      };
+    case PATCH_AUDITION:
+      console.log('PATCHING AUDITION');
+      let stateAuditionCopy = [...state.auditions];
+      let roleIndexArray = stateAuditionCopy.map((role) =>
+        role.auditions.findIndex(
+          (audition) => audition.id === action.payload.id
+        )
+      );
+      let roleIndex = roleIndexArray.findIndex((index) => index > -1);
+      let foundAuditionIndex = stateAuditionCopy[roleIndex].auditions.findIndex(
+        (audition) => audition.id === action.payload.id
+      );
+      stateAuditionCopy[roleIndex].auditions.splice(
+        foundAuditionIndex,
+        1,
+        action.payload
+      );
+      return {
+        ...state,
+        auditions: stateAuditionCopy,
       };
     default:
       return state;
