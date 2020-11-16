2d1e554d0b7a9a16a62db793cf9924d1e768e42b
Jake Mills <jake.mills.05@gmail.com>
2020-11-16 17:19:35 -0500

    Added video overlay for homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,24 +2,30 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import '../styles/home.css';
+import video from '../helpers/NYC Streets.mp4';
 
 export default function Home() {
   return (
-    <div className='row no-gutters'>
-      <div className='col-md-6 no-gutters'>
-        <div className='left d-flex flex-column justify-content-center align-items-center'>
-          <h1>Actors</h1>
-          <Link to='/actor/registration'>
-            <Button variant='primary'>Register Here</Button>
-          </Link>
+    <div className='overlay'>
+      <video autoPlay muted loop id='myVideo'>
+        <source src={video} type='video/mp4' />
+      </video>
+      <div className='row no-gutters'>
+        <div className='col-md-6 no-gutters'>
+          <div className='left d-flex flex-column justify-content-center align-items-center'>
+            <h1>Actors</h1>
+            <Link to='/actor/registration'>
+              <Button variant='primary'>Register Here</Button>
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className='col-md-6 no-gutters'>
-        <div className='right d-flex flex-column justify-content-center align-items-center'>
-          <h1>Casting Directors</h1>
-          <Link to='/casting_director/registration'>
-            <Button variant='danger'>Register Here</Button>
-          </Link>
+        <div className='col-md-6 no-gutters'>
+          <div className='right d-flex flex-column justify-content-center align-items-center'>
+            <h1>Casting Directors</h1>
+            <Link to='/casting_director/registration'>
+              <Button variant='danger'>Register Here</Button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
